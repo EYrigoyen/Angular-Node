cf0e138d8d08ec8dd9788f9d8122a09e20b4f5c1
Eduardo Yrigoyen <eyrigoyen78@gmail.com>
2022-10-13 08:53:14 -0500

    Todo el aplicativo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Componentes
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+
+//Guard
+import { AuthGuard } from './utils/auth.guard';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signIn', component: SignInComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
